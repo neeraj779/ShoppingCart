c20073d37fa8bd4714060f826511b5580e851242
Neeraj <neeraj79108@gmail.com>
2024-09-25 14:08:38 +0530

    Refactor NavBar component to update link styles and add shadow

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
 
 const NavBar = () => (
-  <nav className="p-4 bg-gray-800">
-    <ul className="flex space-x-4">
+  <nav className="p-4 bg-gray-800 shadow-lg">
+    <ul className="flex space-x-6">
       <li>
-        <Link to="/" className="text-white hover:underline">
+        <Link
+          to="/"
+          className="text-lg font-semibold text-white transition duration-200 ease-in-out hover:text-gray-300"
+        >
           Home
         </Link>
       </li>
       <li>
-        <Link to="/cart" className="text-white hover:underline">
+        <Link
+          to="/cart"
+          className="text-lg font-semibold text-white transition duration-200 ease-in-out hover:text-gray-300"
+        >
           Cart
         </Link>
       </li>
